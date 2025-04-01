1a1ddd607ad87c6b59dd397106a86b399dc468a3
djordjevicm93 <djordjevic083@gmail.com>
2025-04-01 23:04:26 +0200

    second try

--- n_g_g.py
+++ n_g_g.py
@@ -2,19 +2,24 @@ from random import randint
 
 draw_number = randint(1, 101)
 print(draw_number)
-guess_number = input("Guess the number: ")
+guess_number_str = input("Guess the number: ")
+guessed = False
 
-def ngg_1 (guess_number):
+while not guessed:
     try:
-        guess_number = int(guess_number)
-    except TypeError:
+        guess_number_int = int(guess_number_str)
+        if guess_number_int < draw_number:
+            print("Too small!")
+            #continue
+        if guess_number_int > draw_number:
+            print("Too big!")
+        if guess_number_int == draw_number:
+            print("You win!")
+            guessed = True
+            break
+        #else:
+            #pass
+    except Exception:
         print("Its not a number!")
-        return guess_number
-
-if guess_number < draw_number:
-    print("Too small!")
-if guess_number > draw_number:
-    print("Too big!")
-if guess_number == draw_number:
-    print("You win!")
+        print(guess_number_str)
         
